e4382a149da6ecaccac98b8d286494fca75590e8
sasikiran <sasikiranessamaraju@gmail.com>
2021-12-23 22:43:27 +0530

    add program to reverse a string using stack data structure.

---
+++ stacks/reversingString.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Reverse a string using stack.
+string reverseString(string &str) {
+    string ans;
+    stack<char> st;
+    for (char c : str) {
+        st.push(c);
+    }
+    while (!st.empty()) {
+        char c = st.top();
+        st.pop();
+        ans.push_back(c);
+    }
+    return ans;
+}
+
+int main() {
+    string name = "Sasi Kiran";
+    cout << reverseString(name) << "\n";
+    return 0;
+}
