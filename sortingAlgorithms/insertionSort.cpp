ca389a25ff859b1cd8b60e4048d80bb8859302d1
sasikiran <sasikiranessamaraju@gmail.com>
2021-12-23 08:09:04 +0530

    add insertion sort algo.

---
+++ sortingAlgorithms/insertionSort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void insertionSort(vector<int> &arr) {
+	int n = arr.size();
+	for (int i = 1; i < n; i++) {
+		int curr = arr[i];
+		int j = i - 1;
+		while (j >= 0 and curr < arr[j]) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = curr;
+	}
+}
+
+
+int main() {
+	vector<int> v = {9, 8, 3, 3, 2, -1, 6, 5, 3, 4, 2};
+	insertionSort(v);
+	for (auto &e : v) {
+		cout << e << " ";
+	}
+	cout << "\n";
+	return 0;
+}
