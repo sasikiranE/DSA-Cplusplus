195bfc4fbab3bba76597cd0aec674d996bedc0e2
sasikiran <sasikiranessamaraju@gmail.com>
2021-12-23 07:29:48 +0530

    refactor code - move all private methods one side.

--- trees/binarySearchTree.cpp
+++ trees/binarySearchTree.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 class BinarySearchTree {
-private:
 
 	class Node {
 	public:
+
 		int data;
 		Node* left;
 		Node* right;
@@ -17,7 +17,9 @@ private:
 	};
 
 	Node* root;
+
 public:
+
 	BinarySearchTree() {
 		root = NULL;
 	}
@@ -66,12 +68,6 @@ public:
 		return height(root);
 	}
 
-	int height(Node* root) {
-		if (root == NULL) return -1;
-		if (root->left == NULL and root->right == NULL) return 0;
-		return 1 + max(height(root->left), height(root->right));
-	}
-
 	int getMin() {
 		if (root == NULL) {
 			return INT_MAX;
@@ -100,41 +96,14 @@ public:
 		cout << "\n";
 	}
 
-	void preOrder(Node* root) {
-		if (root == NULL) {
-			return;
-		}
-		cout << root->data << " ";
-		preOrder(root->left);
-		preOrder(root->right);
-	}
-
 	void inOrder() {
 		inOrder(root);
 		cout << "\n";
 	}
 
-	void inOrder(Node* root) {
-		if (root == NULL) {
-			return;
-		}
-		inOrder(root->left);
-		cout << root->data << " ";
-		inOrder(root->right);
-	}
-
 	void postOrder() {
 		postOrder(root);
 		cout << "\n";
-	}
-
-	void postOrder(Node* root) {
-		if (root == NULL) {
-			return;
-		}
-		postOrder(root->left);
-		postOrder(root->right);
-		cout << root->data << " ";
 	}
 
 	// Breadth First traversal
@@ -152,10 +121,45 @@ public:
 		cout << "\n";
 	}
 
+private:
+
+	int height(Node* root) {
+		if (root == NULL) return -1;
+		if (root->left == NULL and root->right == NULL) return 0;
+		return 1 + max(height(root->left), height(root->right));
+	}
+
+	void preOrder(Node* root) {
+		if (root == NULL) {
+			return;
+		}
+		cout << root->data << " ";
+		preOrder(root->left);
+		preOrder(root->right);
+	}
+
+	void postOrder(Node* root) {
+		if (root == NULL) {
+			return;
+		}
+		postOrder(root->left);
+		postOrder(root->right);
+		cout << root->data << " ";
+	}
+
+	void inOrder(Node* root) {
+		if (root == NULL) {
+			return;
+		}
+		inOrder(root->left);
+		cout << root->data << " ";
+		inOrder(root->right);
+	}
+
 };
 
 int main() {
-	// BinarySearchTree bst = BinarySearchTree();
+	BinarySearchTree bst = BinarySearchTree();
 	// bst.insert(7);
 	// bst.insert(4);
 	// bst.insert(9);
@@ -163,6 +167,5 @@ int main() {
 	// bst.insert(5);
 	// bst.insert(8);
 	// bst.insert(10);
-	// bst.levelOrder();
 	return 0;
 }
